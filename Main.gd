bb1ae6efac08081bdb5729acc22b8205f2cd77a4
JeZxLee <Admin@FallenAngelSoftware.com>
2020-07-21 18:21:25 -0400

    0.0.0.4

--- Main.gd
+++ Main.gd
@@ -13,17 +13,48 @@ extends Node
 
 #----------------------------------------------------------------------------------------
 func _ready():
-	
-	VisualsCore.DrawSprite(1, VisualsCore.ScreenWidth/2, VisualsCore.ScreenHeight/2, 2.845, 1, 0, 1, 1, 1, 1)
 
-	VisualsCore.DrawSprite(2, VisualsCore.ScreenWidth/2, 90, 2, 1, 0, 1, 1, 1, 1)
+# Below is just a temporary test of the new VisualsCore:
 
-	VisualsCore.DrawSprite(3, VisualsCore.ScreenWidth/2, 160, 2.85, 2, 0, 1, 1, 1, 1)
+	VisualsCore.DrawSprite(10, VisualsCore.ScreenWidth/2, VisualsCore.ScreenHeight/2, 2.845, 1.0, 0, 1.0, 1.0, 1.0, 1.0)
+
+	VisualsCore.DrawText(VisualsCore.TextCurrentIndex, "Version 2 - Alpha .0.0.1", 0, 16, 1, 25, 1.0, 1.0, 0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0)
+
+	VisualsCore.DrawSprite(20, VisualsCore.ScreenWidth/2, 90, 1.75, 1.0, 0, 1.0, 1.0, 1.0, 1.0)
+
+	VisualsCore.DrawSprite(30, VisualsCore.ScreenWidth/2, 160, 2.85, 2.0, 0, 1.0, 1.0, 1.0, 1.0)
+
+
+	VisualsCore.TestTextScaleX = 1.0
+	VisualsCore.TestTextScaleY = 1.0
+	VisualsCore.TestTextRotation = 1
+	VisualsCore.DrawText(VisualsCore.TextCurrentIndex, "DEMO", 0, VisualsCore.ScreenHeight/2, 1, 60, 1.0, 1.0, 0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0)
+
+
+	VisualsCore.DrawSprite(31, VisualsCore.ScreenWidth/2, 596, 2.85, 2.0, 0, 1.0, 1.0, 1.0, 1.0)
+
+	VisualsCore.DrawText(VisualsCore.TextCurrentIndex, "©2020 By Team ''www.FallenAngelSoftware.com''", 0, 640-19, 1, 25, 1.0, 1.0, 0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0)
 
 	
 	pass
 
 #----------------------------------------------------------------------------------------
 func _process(delta):
+
+# Below is just a temporary test of the new VisualsCore:
+
+	if VisualsCore.TestTextScaleX < 5.0:
+		VisualsCore.TestTextScaleX+=0.1
+	else: VisualsCore.TestTextScaleX = 0.0
+
+	if VisualsCore.TestTextScaleY < 5.0:
+		VisualsCore.TestTextScaleY+=0.1
+	else: VisualsCore.TestTextScaleY = 0.0
+
+	if VisualsCore.TestTextRotation < 360:
+		VisualsCore.TestTextRotation+=1
+	else: VisualsCore.TestTextRotation = 0
+
+	VisualsCore.DrawnTextChangeScaleRotation(1, VisualsCore.TestTextScaleX, VisualsCore.TestTextScaleY, VisualsCore.TestTextRotation)
 
 	pass
--- VisualsCore.gd
+++ VisualsCore.gd
@@ -18,24 +18,32 @@ class SpriteClass:
 	var SpriteColorAlpha = []
 var Sprites = SpriteClass.new()
 
-#class TextClass:
-#	var TextImage = []
-#	var TextIndex = []
-#	var TextScreenX = []
-#	var TextScreenY = []
-#	var TextHorizontalJustification = []
-#	var TextSize = []
-#	var TextScaleX = []
-#	var TextScaleY = []
-#	var TextRotation = []
-#	var TextColorRed  = []
-#	var TextColorGreen = []
-#	var TextColorBlue = []
-#	var TextColorAlpha = []
-#	var TextOutlineRed = []
-#	var TextOutlineGreen = []
-#	var TextOutlineBlue = []
-#var Texts = TextClass.new()
+var FontTTF = []
+
+var TextCurrentIndex;
+
+class TextClass:
+	var TextImage = []
+	var TextIndex = []
+	var TextScreenX = []
+	var TextScreenY = []
+	var TextHorizontalJustification = []
+	var TextSize = []
+	var TextScaleX = []
+	var TextScaleY = []
+	var TextRotation = []
+	var TextColorRed  = []
+	var TextColorGreen = []
+	var TextColorBlue = []
+	var TextColorAlpha = []
+	var TextOutlineRed = []
+	var TextOutlineGreen = []
+	var TextOutlineBlue = []
+var Texts = TextClass.new()
+
+var TestTextScaleX = 1.0
+var TestTextScaleY = 1.0
+var TestTextRotation = 0
 
 
 #----------------------------------------------------------------------------------------
@@ -56,20 +64,31 @@ func _ready():
 	Sprites.SpriteImage[0].set_texture(preload("res://media/images/backgrounds/FadingBlackBG.png"))
 	Sprites.SpriteActive[0] = true
 
-	Sprites.SpriteImage[1].set_texture(preload("res://media/images/backgrounds/TitleBG.png"))
-	Sprites.SpriteActive[1] = true
+	Sprites.SpriteImage[10].set_texture(preload("res://media/images/backgrounds/TitleBG.png"))
+	Sprites.SpriteActive[10] = true
 	
-	Sprites.SpriteImage[2].set_texture(preload("res://media/images/logos/Logo.png"))
-	Sprites.SpriteActive[2] = true
+	Sprites.SpriteImage[20].set_texture(preload("res://media/images/logos/Logo.png"))
+	Sprites.SpriteActive[20] = true
 
-	Sprites.SpriteImage[3].set_texture(preload("res://media/images/gui/ScreenLine.png"))
-	Sprites.SpriteActive[3] = true
+	Sprites.SpriteImage[30].set_texture(preload("res://media/images/gui/ScreenLine.png"))
+	Sprites.SpriteActive[30] = true
+	Sprites.SpriteImage[31].set_texture(preload("res://media/images/gui/ScreenLine.png"))
+	Sprites.SpriteActive[31] = true
 
 	
 	for index in range(0, 1000):
 		if Sprites.SpriteActive[index] == true:
 			add_child(Sprites.SpriteImage[index])
 			Sprites.SpriteImage[index].global_position = Vector2(Sprites.SpriteScreenX[index], Sprites.SpriteScreenY[index])
+
+	FontTTF.append(DynamicFont.new())
+	FontTTF[0].font_data = load("res://media/fonts/LiberationSans-Regular.ttf")
+	FontTTF[0].size = 25
+	FontTTF.append(DynamicFont.new())
+	FontTTF[1].font_data = load("res://media/fonts/LiberationSans-Regular.ttf")
+	FontTTF[1].size = 60
+
+	TextCurrentIndex = 0
 
 	pass
 
@@ -94,51 +113,76 @@ func DrawSprite(var index, var x, var y, var scaleX, var scaleY, var rotation, v
 	pass
 
 #----------------------------------------------------------------------------------------
-func DrawText(var index, var x, var y, var horizontalJustification, var size, var scaleX, var scaleY, var rotation, var red, var green, var blue, var alpha, var outlineRed, var outlineGreen, var outlineBlue):
+func DrawnTextChangeScaleRotation(var index, var scaleX, var scaleY, var rotation):
+	VisualsCore.Texts.TextImage[index].rect_scale = Vector2(scaleX, scaleY)
+	VisualsCore.Texts.TextImage[index].rect_rotation = rotation
 
-#	var RTL = RichTextLabel.new()
-#
-#	var FONT = DynamicFont.new()
-#	FONT.font_data = load("res://media/fonts/LiberationSans-Regular.ttf")
-#	FONT.size = 25
-#
-#
-#	for index in range(0, 1000):
-#		Texts.TextImage[index].append(null)
+	pass
 
-#	var TextImage = []
-#	var TextIndex = []
-#	var TextScreenX = []
-#	var TextScreenY = []
-#	var TextHorizontalJustification = []
-#	var TextSize = []
-#	var TextScaleX = []
-#	var TextScaleY = []
-#	var TextRotation = []
-#	var TextColorRed = []
-#	var TextColorGreen = []
-#	var TextColorBlue = []
-#	var TextColorAlpha = []
-#	var TextOutlineRed = []
-#	var TextOutlineGreen = []
-#	var TextOutlineBlue = []
-#var Texts = TextClass.new()
+#----------------------------------------------------------------------------------------
+func DrawText(var index, var text, var x, var y, var horizontalJustification, var fontSize, var scaleX, var scaleY, var rotation, var red, var green, var blue, var alpha, var outlineRed, var outlineGreen, var outlineBlue):
+	Texts.TextImage.append(RichTextLabel.new())
 
-#	if Sprites.SpriteActive[index] == false:  return false
-#	
-#	Sprites.SpriteImage[index].global_position = Vector2(x, y)
-#	Sprites.SpriteImage[index].scale = Vector2(scaleX, scaleY)
-#	Sprites.SpriteImage[index].rotation = deg2rad(rotation)
-#	Sprites.SpriteImage[index].modulate = Color(red, green, blue, alpha)
-#
-#	Sprites.SpriteScreenX[index] = x
-#	Sprites.SpriteScreenY[index] = y
-#	Sprites.SpriteScaleX[index] = scaleX
-#	Sprites.SpriteScaleY[index] = scaleY
-#	Sprites.SpriteRotation[index] = rotation
-#	Sprites.SpriteColorRed[index] = red
-#	Sprites.SpriteColorGreen[index] = green
-#	Sprites.SpriteColorBlue[index] = blue
-#	Sprites.SpriteColorAlpha[index] = alpha
+	add_child(Texts.TextImage[index])
+
+	var fontToUseIndex = 0
+	if fontSize == 25:
+		fontToUseIndex = 0
+	elif fontSize == 60:
+		fontToUseIndex = 1
+
+	Texts.TextImage[index].add_font_override("normal_font", FontTTF[fontToUseIndex])
+
+	Texts.TextImage[index].modulate = Color(red, green, blue, alpha)
+	
+	FontTTF[fontToUseIndex].outline_size = 3
+	FontTTF[fontToUseIndex].outline_color = Color(outlineRed, outlineGreen, outlineBlue, alpha)
+
+	Texts.TextImage[index].text = text
+
+	var textWidth = Texts.TextImage[index].get_font("normal_font").get_string_size(Texts.TextImage[index].text).x
+	var textHeight = Texts.TextImage[index].get_font("normal_font").get_string_size(Texts.TextImage[index].text).y
+
+	Texts.TextImage[index].rect_global_position.x = x
+	Texts.TextImage[index].rect_global_position.y = (y - (textHeight / 2))
+
+	Texts.TextImage[index].set_size(Vector2(ScreenWidth, ScreenHeight), false)
+
+	Texts.TextImage[index].rect_pivot_offset = Vector2((textWidth / 2), (textHeight / 2))
+
+	Texts.TextImage[index].rect_scale = Vector2(scaleX, scaleY)
+
+	Texts.TextImage[index].rect_rotation = rotation
+
+	if horizontalJustification == 0:
+		Texts.TextImage[index].ALIGN_LEFT
+	elif horizontalJustification == 1:
+		Texts.TextImage[index].ALIGN_CENTER
+		Texts.TextImage[index].rect_global_position.x = ((VisualsCore.ScreenWidth/2) - (textWidth / 2))
+	elif horizontalJustification == 2:
+		Texts.TextImage[index].ALIGN_LEFT
+		Texts.TextImage[index].rect_global_position.x = (VisualsCore.ScreenWidth - x - (textWidth))
+	elif horizontalJustification == 4:
+		Texts.TextImage[index].ALIGN_LEFT
+		Texts.TextImage[index].rect_global_position.x = (x - (textWidth / 2))
+	else:  Texts.TextImage[index].ALIGN_FILL
+
+	Texts.TextIndex.append(index)
+	Texts.TextScreenX.append(x)
+	Texts.TextScreenY.append(y)
+	Texts.TextHorizontalJustification.append(horizontalJustification)
+	Texts.TextSize.append(fontSize)
+	Texts.TextScaleX.append(scaleX)
+	Texts.TextScaleY.append(scaleY)
+	Texts.TextRotation.append(rotation)
+	Texts.TextColorRed.append(red)
+	Texts.TextColorGreen.append(green)
+	Texts.TextColorBlue.append(blue)
+	Texts.TextColorAlpha.append(alpha)
+	Texts.TextOutlineRed.append(outlineRed)
+	Texts.TextOutlineGreen.append(outlineGreen)
+	Texts.TextOutlineBlue.append(outlineBlue)
+
+	TextCurrentIndex+=1
 
 	pass
