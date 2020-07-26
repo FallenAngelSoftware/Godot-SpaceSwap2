5f143cd2927d91af426c3e2b3b411d7b980a2bfb
JeZxLee <Admin@FallenAngelSoftware.com>
2020-07-26 13:17:32 -0400

    0.0.0.32

--- AudioCore.gd
+++ AudioCore.gd
@@ -6,32 +6,57 @@ var MusicVolume
 var EffectsVolume
 
 var MusicPlayer = []
+var MusicTotal = 11
 var MusicCurrentlyPlaying = -1
 
 var EffectPlayer = []
+var EffectsTotal = 12
 
 
 func _ready():
 	MusicVolume = 1.0
 	EffectsVolume = 1.0
 
-	MusicPlayer.append(AudioStreamPlayer.new())
-	MusicPlayer[0].stream = load("res://media/music/TitleBGM.ogg")
-	add_child(MusicPlayer[0])
-	MusicPlayer[0].set_volume_db(linear2db(MusicVolume))
-	MusicPlayer[0].stream.set_loop(true)
+	for index in range(0, MusicTotal):
+		MusicPlayer.append(AudioStreamPlayer.new())
+		if index == 0:  MusicPlayer[index].stream = load("res://media/music/TitleBGM.ogg")
+		elif index == 1:  MusicPlayer[index].stream = load("res://media/music/Title2BGM.ogg")
+		elif index == 2:  MusicPlayer[index].stream = load("res://media/music/StoryIntroBGM.ogg")
+		elif index == 3:  MusicPlayer[index].stream = load("res://media/music/InGameStartBGM.ogg")
+		elif index == 4:  MusicPlayer[index].stream = load("res://media/music/InGameMiddleBGM.ogg")
+		elif index == 5:  MusicPlayer[index].stream = load("res://media/music/InGameEndBGM.ogg")
+		elif index == 6:  MusicPlayer[index].stream = load("res://media/music/TimeAttackBGM.ogg")
+		elif index == 7:  MusicPlayer[index].stream = load("res://media/music/NewHighScoreBGM.ogg")
+		elif index == 8:  MusicPlayer[index].stream = load("res://media/music/TophighScoreBGM.ogg")
+		elif index == 9:  MusicPlayer[index].stream = load("res://media/music/Ending2BGM.ogg")
+		elif index == 10:  MusicPlayer[index].stream = load("res://media/music/WinTurbo2BGM.ogg")
+		add_child(MusicPlayer[index])
+		MusicPlayer[index].set_volume_db(linear2db(MusicVolume))
+		MusicPlayer[index].stream.set_loop(true)
 
-	EffectPlayer.append(AudioStreamPlayer.new())
-	EffectPlayer[0].stream = load("res://media/sound/MenuMove.ogg")
-	add_child(EffectPlayer[0])
-	EffectPlayer[0].set_volume_db(linear2db(EffectsVolume))
-	EffectPlayer[0].stream.set_loop(false)
+	for index in range(0, EffectsTotal):
+		EffectPlayer.append(AudioStreamPlayer.new())
+		if index == 0:  EffectPlayer[index].stream = load("res://media/sound/MenuMove.ogg")
+		elif index == 1:  	EffectPlayer[index].stream = load("res://media/sound/MenuClick.ogg")
+		elif index == 2:  	EffectPlayer[index].stream = load("res://media/sound/Burp.ogg")
+		elif index == 3:  	EffectPlayer[index].stream = load("res://media/sound/Thrust.ogg")
+		elif index == 4:  	EffectPlayer[index].stream = load("res://media/sound/MoveSelector.ogg")
+		elif index == 5:  	EffectPlayer[index].stream = load("res://media/sound/SwapPieces.ogg")
+		elif index == 6:  	EffectPlayer[index].stream = load("res://media/sound/PiecesCleared.ogg")
+		elif index == 7:  	EffectPlayer[index].stream = load("res://media/sound/MultiDropClear.ogg")
+		elif index == 8:  	EffectPlayer[index].stream = load("res://media/sound/PlayfieldRise.ogg")
+		elif index == 9:  	EffectPlayer[index].stream = load("res://media/sound/LevelUp.ogg")
+		elif index == 10:  	EffectPlayer[index].stream = load("res://media/sound/GameOver.ogg")
+		elif index == 11:  	EffectPlayer[index].stream = load("res://media/sound/Explosion.ogg")
+		add_child(EffectPlayer[index])
+		EffectPlayer[index].set_volume_db(linear2db(EffectsVolume))
+		EffectPlayer[index].stream.set_loop(false)
 
-	EffectPlayer.append(AudioStreamPlayer.new())
-	EffectPlayer[1].stream = load("res://media/sound/MenuClick.ogg")
-	add_child(EffectPlayer[1])
-	EffectPlayer[1].set_volume_db(linear2db(EffectsVolume))
-	EffectPlayer[1].stream.set_loop(false)
+	pass
+
+
+func SetMusicAndEffectsVolume(var musicVolume, var effectsVolume):
+
 
 	pass
 
